572e765f2c33e5ab32d944ac9b222b3465b437c8
John Werenski <jtwerenski@gmail.com>
2018-09-16 15:25:32 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -40,6 +40,7 @@ def monopoly_with_third_tier
   monopoly[:railroads][:names][:pennsylvania_railroad] = {}
   monopoly[:railroads][:names][:b_and_o_railroad] = {}
   monopoly[:railroads][:names][:shortline_railroad] = {}
+  monopoly
 end
 
 def monopoly_with_fourth_tier
