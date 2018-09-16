3f6e715de74c75d6419e5fad84eb584a19c40994
John Werenski <jtwerenski@gmail.com>
2018-09-16 15:23:50 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,6 +36,7 @@ def monopoly_with_third_tier
   monopoly[:railroads][:rent_in_dollars][:three_pieces_owned] 
     = 100
   monopoly[:railroads][:rent_in_dollars][:four_pieces_owned]     = 200
+  monopoly[:railroads][:names][:reading_railroad] = {}
 end
 
 def monopoly_with_fourth_tier
